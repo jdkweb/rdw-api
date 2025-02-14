c2506a3e238c6d60214947ae8af25b2b37a21cfa
Jasper <jasper@jdkweb.nl>
2025-02-14 15:41:53 +0100

    New Hack for Enums Filament interface and namespaces and composer dump-autoload problems

--- src/Enums/Endpoints.php
+++ src/Enums/Endpoints.php
@@ -1,20 +1,19 @@
 <?php
 
-# namespace Jdkweb\RdwApi\Enums;
-
 /**
  * Hack for loading an enum using the getLabel method that works in both laravel and Filament
  *
  * If rdw-api is used without Filament the HasLabel implements will give an error.
  *
+ * ! Composer dump-autoload wil not accept this file without class / namespace
+ *
  * extends is not allowed in enums
  */
 if (! (\Composer\InstalledVersions::isInstalled('filament/filament')) ) {
-    //require_once 'EndpointsLaravel.php';
     require_once 'parent/Endpoints.php';
 }
 elseif((\Composer\InstalledVersions::isInstalled('jdkweb/rdw-api-filament'))) {
-    $path = app()->basePath('/vendor/jdkweb/rdw-api-filament/src/Enums') . '/EndpointsFilament.php';
+    $path = app()->basePath('/vendor/jdkweb/rdw-api-filament/src/Enums') . '/Endpoints.php';
     if(file_exists($path)) {
         require_once $path;
     }
--- src/Enums/OutputFormat.php
+++ src/Enums/OutputFormat.php
@@ -1,20 +1,20 @@
 <?php
 
-# namespace Jdkweb\RdwApi\Enums;
-
 /**
  * Hack for loading an enum using the getLabel method that works in both laravel and Filament
  *
  * If rdw-api is used without Filament the HasLabel implements will give an error.
  *
+ * ! Composer dump-autoload wil not accept this file without class / namespace
+ *
  * extends is not allowed in enums
  */
 if (! (\Composer\InstalledVersions::isInstalled('filament/filament')) ) {
-    require_once 'parent/OutputFormatLaravel.php';
+    require_once 'parent/OutputFormat.php';
 }
 elseif((\Composer\InstalledVersions::isInstalled('jdkweb/rdw-api-filament'))) {
-    $path = app()->basePath('/vendor/jdkweb/rdw-api-filament/src/Enums') . '/OutputFormatFilament.php';
+    $path = app()->basePath('/vendor/jdkweb/rdw-api-filament/src/Enums') . '/OutputFormat.php';
     if(file_exists($path)) {
-        require_once app()->basePath('/vendor/jdkweb/rdw-api-filament/src/Enums') . '/OutputFormatFilament.php';
+        require_once $path;
     }
 }
