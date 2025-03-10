fb688d1bdf27358a414fe1d8aabfda0d76ed398c
Freek <github@freekdk.nl>
2025-03-10 11:57:25 +0100

    remove unneeded declarations

--- src/Enums/OutputFormat.php
+++ src/Enums/OutputFormat.php
@@ -3,35 +3,12 @@ namespace Jdkweb\RdwApi\Enums;
 
 use Filament\Support\Contracts\HasLabel;
 
-/**
- * Hack for loading an enum using the getLabel method that works in both laravel and Filament
- *
- * If rdw-api is used without Filament the HasLabel implements will give an error.
- *
- * ! Composer dump-autoload wil not accept this file without class / namespace
- *
- * extends is not allowed in enums
- */
-if( (\Composer\InstalledVersions::isInstalled('filament/filament')) &&
-    (\Composer\InstalledVersions::isInstalled('jdkweb/rdw-api-filament'))) {
+enum OutputFormat: string
+{
+    use OutputFormatTrait;
 
-    enum OutputFormat: string implements HasLabel
-    {
-        use OutputFormatTrait;
-
-        case ARRAY = 'array';
-        case JSON = 'json';
-        case XML = 'xml';
-    }
+    case ARRAY = 'array';
+    case JSON = 'json';
+    case XML = 'xml';
 }
-else {
 
-    enum OutputFormat: string
-    {
-        use OutputFormatTrait;
-
-        case ARRAY = 'array';
-        case JSON = 'json';
-        case XML = 'xml';
-    }
-}
